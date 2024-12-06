cb4782377681dac7dd0bb8709ad3039fd1767734
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2024-12-06 23:33:49 +0300

    Update models.py

--- interior_health/users/models.py
+++ interior_health/users/models.py
@@ -1,3 +1,17 @@
+# users/models.py
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('health_worker', 'Health Worker'),
+        ('patient', 'Patient'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='patient')
+    address = models.CharField(max_length=255, blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+

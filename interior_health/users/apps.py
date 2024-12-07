6ed4a9cb00a1a930a96fdf4919bb1ecee37b3f2c
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2024-12-07 13:23:09 +0300

    Update apps.py

--- interior_health/users/apps.py
+++ interior_health/users/apps.py
@@ -1,11 +1,11 @@
 from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
-    name = 'users'
+    name = 'users'  # This specifies the name of your app. Make sure it matches the directory name of your app.
 
     def ready(self):
-        
+        # This is where you'd put any code that needs to run when the app is ready.
+        # Since you're not using signals, we can leave this empty for now.
+        pass
 
-
-    
 

2eeec40aaf73bdd320493ed4be24890fafcb0a2c
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2024-12-07 13:17:47 +0300

    Update apps.py

--- interior_health/users/apps.py
+++ interior_health/users/apps.py
@@ -4,6 +4,5 @@ class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
 
-    def ready(self):
-        import users.signals  # If signals are needed, otherwise omit
+    
 
